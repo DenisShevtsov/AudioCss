32d31fda4536e1cc048d02d48e59b7c3a9523967
Denis <bagudila220@gmail.com>
2018-12-17 17:21:33 +0400

    2

--- app/src/main/java/ru/startandroid/audiocross/MainActivity.java
+++ app/src/main/java/ru/startandroid/audiocross/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     TextView timer;
     SeekBar sBar;
     int mix = 2;
-    MediaPlayer mediaPlayer1, mediaPlayer2;
     AudioManager am;
 
     @Override
@@ -46,59 +45,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         try {
-        switch (view.getId()) {
-            case R.id.audio1:
-                OpenFileDialog fileDialog = new OpenFileDialog(this)
-                        .setFilter(".*\\.mp3")
-                        .setOpenDialogListener(new OpenFileDialog.OpenDialogListener() {
-                            @Override
-                            public void OnSelectedFile(String fileName) {
-                                musik1 = fileName;
-                            }
-                        });
-                fileDialog.show();
-                break;
+            switch (view.getId()) {
+                case R.id.audio1:
+                    OpenFileDialog fileDialog = new OpenFileDialog(this)
+                            .setFilter(".*\\.mp3")
+                            .setOpenDialogListener(new OpenFileDialog.OpenDialogListener() {
+                                @Override
+                                public void OnSelectedFile(String fileName) {
+                                    musik1 = fileName;
+                                }
+                            });
+                    fileDialog.show();
+                    break;
 
-            case R.id.audio2:
-                OpenFileDialog fileDialog2 = new OpenFileDialog(this)
-                        .setFilter(".*\\.mp3")
-                        .setOpenDialogListener(new OpenFileDialog.OpenDialogListener() {
-                            @Override
-                            public void OnSelectedFile(String fileName) {
-                                musik2 = fileName;
-                            }
-                        });
-                fileDialog2.show();
-                break;
+                case R.id.audio2:
+                    OpenFileDialog fileDialog2 = new OpenFileDialog(this)
+                            .setFilter(".*\\.mp3")
+                            .setOpenDialogListener(new OpenFileDialog.OpenDialogListener() {
+                                @Override
+                                public void OnSelectedFile(String fileName) {
+                                    musik2 = fileName;
+                                }
+                            });
+                    fileDialog2.show();
+                    break;
 
-            case R.id.play:
-                mediaPlayer1 = new MediaPlayer();
-                mediaPlayer2 = new MediaPlayer();
-                mediaPlayer1.setDataSource(musik1);
-                mediaPlayer2.setDataSource(musik2);
-                mediaPlayer1.setLooping(true);
-                mediaPlayer2.setLooping(true);
-                mediaPlayer1.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                mediaPlayer2.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                mediaPlayer1.prepare();
-                mediaPlayer2.prepare();
-
-
-                mediaPlayer1.start();
-                for (int g = mix;;){
-                    if(mediaPlayer1.getCurrentPosition() > g*1000){
-                        mediaPlayer1.pause();
-                        mediaPlayer2.start();
-                        if (mediaPlayer2.getCurrentPosition() > g*1000){
-                            mediaPlayer2.pause();
-                            mediaPlayer1.start();
-                            g = g + mix;
-                        }
-                    }
-                }
-        }
-        } catch (IOException exc) {
-            Toast.makeText(this, exc.toString(), Toast.LENGTH_LONG).show();
+                case R.id.play:
+                    PleyThread pl = new PleyThread(musik1, musik2, mix, "Play");
+                    break;
+            }
         }catch (RuntimeException exc) {
             Toast.makeText(this, exc.toString(), Toast.LENGTH_LONG).show();
         }
