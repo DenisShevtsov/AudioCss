dc8d2075e6d35618331aa3b07df73df4e8f131ae
Denis <bagudila220@gmail.com>
2018-12-17 16:28:29 +0400

    2

--- app/src/main/java/ru/startandroid/audiocross/MainActivity.java
+++ app/src/main/java/ru/startandroid/audiocross/MainActivity.java
@@ -1,14 +1,26 @@
 package ru.startandroid.audiocross;
 
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, SeekBar.OnSeekBarChangeListener {
 
     Button audio1, audio2, play;
+    private String musik1, musik2;
+    TextView timer;
+    SeekBar sBar;
+    int mix = 2;
+    MediaPlayer mediaPlayer1, mediaPlayer2;
+    AudioManager am;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,25 +29,102 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         audio1 = (Button) findViewById(R.id.audio1);
         audio2 = (Button) findViewById(R.id.audio2);
+        sBar = (SeekBar) findViewById(R.id.seekBar);
         play = (Button) findViewById(R.id.play);
+        timer = (TextView) findViewById(R.id.timer);
+        am = (AudioManager) getSystemService(AUDIO_SERVICE);
+
 
         audio1.setOnClickListener(this);
         audio2.setOnClickListener(this);
+        sBar.setOnSeekBarChangeListener(this);
+        play.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        try {
+        switch (view.getId()) {
+            case R.id.audio1:
+                OpenFileDialog fileDialog = new OpenFileDialog(this)
+                        .setFilter(".*\\.mp3")
+                        .setOpenDialogListener(new OpenFileDialog.OpenDialogListener() {
+                            @Override
+                            public void OnSelectedFile(String fileName) {
+                                musik1 = fileName;
+                            }
+                        });
+                fileDialog.show();
+                break;
+
+            case R.id.audio2:
+                OpenFileDialog fileDialog2 = new OpenFileDialog(this)
+                        .setFilter(".*\\.mp3")
+                        .setOpenDialogListener(new OpenFileDialog.OpenDialogListener() {
+                            @Override
+                            public void OnSelectedFile(String fileName) {
+                                musik2 = fileName;
+                            }
+                        });
+                fileDialog2.show();
+                break;
+
+            case R.id.play:
+                mediaPlayer1 = new MediaPlayer();
+                mediaPlayer2 = new MediaPlayer();
+                mediaPlayer1.setDataSource(musik1);
+                mediaPlayer2.setDataSource(musik2);
+                mediaPlayer1.setLooping(true);
+                mediaPlayer2.setLooping(true);
+                mediaPlayer1.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                mediaPlayer2.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                mediaPlayer1.prepare();
+                mediaPlayer2.prepare();
+
+
+                mediaPlayer1.start();
+                for (int g = mix;;){
+                    if(mediaPlayer1.getCurrentPosition() > g*1000){
+                        mediaPlayer1.pause();
+                        mediaPlayer2.start();
+                        if (mediaPlayer2.getCurrentPosition() > g*1000){
+                            mediaPlayer2.pause();
+                            mediaPlayer1.start();
+                            g = g + mix;
+                        }
+                    }
+                }
+        }
+        } catch (IOException exc) {
+            Toast.makeText(this, exc.toString(), Toast.LENGTH_LONG).show();
+        }catch (RuntimeException exc) {
+            Toast.makeText(this, exc.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        int progress = i;
+        int st = sBar.getMax()/9;
+
+        for (int r = 1; r < 10; r++){
+            if (progress < r * st) {
+                mix = r + 1;
+                break;
+            }
+        }
+        timer.setText(mix + "c");
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
 
     }
 
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.audio1 :
-                try {
-                    OpenFileDialog fileDialog = new OpenFileDialog(this);
-                    fileDialog.show();
-                } catch (RuntimeException exc) {
-                    Toast.makeText(this, exc.toString(), Toast.LENGTH_LONG).show();
-                };
+    public void onStopTrackingTouch(SeekBar seekBar) {
 
-                break;
-        }
     }
 }
--- app/src/main/java/ru/startandroid/audiocross/OpenFileDialog.java
+++ app/src/main/java/ru/startandroid/audiocross/OpenFileDialog.java
@@ -2,60 +2,70 @@ package ru.startandroid.audiocross;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Environment;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.io.File;
-import java.lang.reflect.Array;
+import java.io.FilenameFilter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+
+
 public class OpenFileDialog extends AlertDialog.Builder {
 
     private String currentPath = Environment.getExternalStorageDirectory().getPath();
-    private  List<File> files = new ArrayList<File>();
+    private List<File> files = new ArrayList<File>();
+    private FilenameFilter filenameFilter;
+    private ListView listView;
+    private int selectedIndex = -1;
+    private OpenDialogListener listener;
 
     public OpenFileDialog(Context context) {
         super(context);
         files.addAll(getFiles(currentPath));
-        ListView listView = createListView(context);
-        listView.setAdapter(new FileAdapter(context, files));
+        listView = createListView(context);
         setView(listView)
-                .setPositiveButton(android.R.string.ok, null)
+                .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int which) {
+                        if (selectedIndex > -1 && listener != null) {
+                            listener.OnSelectedFile(listView.getItemAtPosition(selectedIndex).toString());
+                        }
+                    }
+                })
                 .setNegativeButton(android.R.string.cancel, null);
     }
 
-    private void RebuildFiles (ArrayAdapter<File> adapter) {
-        files.clear();
-        files.addAll(getFiles(currentPath));
-        adapter.notifyDataSetChanged();
+    public interface OpenDialogListener {
+        public void OnSelectedFile(String fileName);
     }
 
-    private ListView createListView(Context context) {
-        ListView listView = new ListView(context);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                final  ArrayAdapter<File> adapter = (FileAdapter) adapterView.getAdapter();
-                File file = adapter.getItem(i);
-                if (file.isDirectory()) {
-                    currentPath = file.getPath();
-                    RebuildFiles(adapter);
-                }
-            }
-        });
-        return listView;
+    public OpenFileDialog setOpenDialogListener(OpenDialogListener listener) {
+        this.listener = listener;
+        return this;
     }
+
+    @Override
+    public AlertDialog show() {
+        files.addAll(getFiles(currentPath));
+        listView.setAdapter(new FileAdapter(getContext(), files));
+        return super.show();
+    }
+
+
     private List<File> getFiles(String directoryPath){
         File directory = new File(directoryPath);
-        File S = directory.getAbsoluteFile();
-        List<File> fileList = Arrays.asList(directory.listFiles());   //nullpointerexception ошибка
+        List<File> fileList = Arrays.asList(directory.listFiles(filenameFilter));
         Collections.sort(fileList, new Comparator<File>() {
             @Override
             public int compare(File file, File file2) {
@@ -69,5 +79,70 @@ public class OpenFileDialog extends AlertDialog.Builder {
         });
         return  fileList;
     }
+
+    public OpenFileDialog setFilter(final  String filter) {
+        filenameFilter = new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                File tempFile = new File(String.format("%s/%s", file.getPath(), s));
+                if (tempFile.isFile())
+                    return tempFile.getName().matches(filter);
+                return true;
+            }
+        };
+        return this;
+    }
+
+
+    private void RebuildFiles (ArrayAdapter<File> adapter) {
+        List<File> fileList = getFiles(currentPath);
+        files.clear();
+        selectedIndex = -1;
+        files.addAll(fileList);
+        adapter.notifyDataSetChanged();
+    }
+
+    private ListView createListView(Context context) {
+        ListView listView = new ListView(context);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                final  ArrayAdapter<File> adapter = (FileAdapter) adapterView.getAdapter();
+                File file = adapter.getItem(i);
+                if (file.isDirectory()) {
+                    currentPath = file.getPath();
+                    RebuildFiles(adapter);
+                } else {
+                    if (i != selectedIndex)
+                        selectedIndex = i;
+                    else
+                        selectedIndex = -1;
+                    adapter.notifyDataSetChanged();
+                }
+            }
+        });
+        return listView;
+    }
+
+
+    public class FileAdapter extends ArrayAdapter<File> {
+        public FileAdapter(Context context, List<File> files){
+            super(context, android.R.layout.simple_expandable_list_item_1, files);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView view = (TextView) super.getView(position, convertView, parent);
+            File file = getItem(position);
+            view.setText(file.getName());
+            if (selectedIndex == position)
+                view.setBackgroundColor(getContext().getResources().getColor(android.R.color.holo_blue_dark));
+            else
+                view.setBackgroundColor(getContext().getResources().getColor(android.R.color.transparent));
+
+            return view;
+        }
+    }
+
 
 }
